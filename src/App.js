6abea7f33ef41161b12919d8e7d53fbcb1a0a2fb
pooja <poojaaa8810@gmail.com>
2026-02-26 12:01:04 +0530

    Cleaned App.js for Vercel deployment

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Reg from "./Reg"
-import Home from "./Home"
+import Reg from "./Reg";
+import Home from "./Home";
+
 function App() {
   return (
-    <>
-     <h1> I am App</h1>
-     <BrowserRouter>
-     <Routes>
-        <Route path="/reg" element={<Reg/>}/>
-        <Route path="/h" element={<Home/>}/>
-     </Routes>
-     </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <h1>I am App</h1>
+      <Routes>
+        <Route path="/reg" element={<Reg />} />
+        <Route path="/h" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
